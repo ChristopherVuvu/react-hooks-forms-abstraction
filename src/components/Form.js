4e270df3e0f843ecfcf684ed0b11f5b9852ed1c1
ChristopherVuvu <vuvumumo93@gmail.com>
2023-04-28 04:18:36 +0300

    added comments

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,26 +7,31 @@ function Form() {
     admin: false,
   })
 
+  //create a function to handle changes
   function handleChange(event) {
     const name = event.target.name;
     let value = event.target.value
 
-    //use 'checked' property for checkboxes instead of 'value'
+    //use 'checked' property for checkboxes instead of 'value'. 
+    //This is to define the checkbox
     if(event.target.type === "checkbox"){
       value = event.target.checked;
     }
 
+    //set a formData to spread and add the new name value
     setFormData({
       ...formData,
       name: value,
       });
   }
 
+  //This function will handle the submit button event
   function handleSubmit(event) {
     event.preventDefault();
     console.log(formData);
   }
 
+  //return the form values and attache the corresponding events
   return (
     <form onSubmit = {handleSubmit}>
       <input 
